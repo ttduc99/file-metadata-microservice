b597ac41f6b11db7d62d980d98c8701f16c65f52
ttduc99 <ttduc999@gmail.com>
2025-09-29 16:51:31 +0700

    file metadata api

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
 var app = express();
+const multer = require('multer');
+const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -10,11 +11,20 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+// API upload file
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.json({ error: 'No file uploaded' });
+  }
 
-
-
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(port, () => {
+  console.log('Server running on port ' + port);
 });
